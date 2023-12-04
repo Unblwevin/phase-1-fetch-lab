c692ce7c993d08502b5693a29c6ab5b08a334638
kevinwilliams <kw119919@gmail.com>
2023-12-04 15:56:28 -0800

    done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => console.log(resp))
+    .catch((error) => console.log(error));
 }
+
+fetchBooks();
 
 function renderBooks(books) {
   const main = document.querySelector('main');
